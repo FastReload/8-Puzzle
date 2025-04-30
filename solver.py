67cb388558c0fa3dba4383d7649d342a9d8ac44d
Aryan Ramachandra <aryanramchandra@outlook.com>
2025-04-30 01:57:17 +0000

    Basic node class and blank finder

--- solver.py
+++ solver.py
@@ -1,4 +1,20 @@
 
+class Node:
+    def __init__(self, state, parent=None, action=None, cost=0, depth=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.cost = cost
+        self.depth = depth
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+def find_blank(state):
+    for i in range(len(state)):
+        for j in range(len(state[0])):
+            if state[i][j] == 0:
+                return i, j
 def solve():
     print("Solving the 8-puzzle!")
 
