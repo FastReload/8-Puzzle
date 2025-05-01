eba0c7e427306b9dc17ce5d9f3249acd2cee24df
Aryan Ramachandra <aryanramchandra@outlook.com>
2025-05-01 00:22:18 +0000

    Basic Node Expansion

--- solver.py
+++ solver.py
@@ -16,3 +16,18 @@ def find_blank(state):
             if state[i][j] == 0:
                 return i, j
 
+def expand(node):
+    directions = {'up': (-1, 0), 'down': (1, 0), 'left': (0, -1), 'right': (0, 1)}
+    blank_i, blank_j = find_blank(node.state)
+    size = len(node.state)
+    children = []
+
+    for move, (di, dj) in directions.items():
+        ni, nj = blank_i + di, blank_j + dj
+        if 0 <= ni < size and 0 <= nj < size:
+            node.state[blank_i][blank_j], node.state[ni][nj] = node.state[ni][nj], node.state[blank_i][blank_j]
+            children.append(Node(node.state, parent=node, action=move, cost=node.cost + 1, depth=node.depth + 1))
+
+    return children
+
+
